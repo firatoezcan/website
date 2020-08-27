d80cbd57b03a5aaa95869c9659c8cc23b140522a
Firat Özcan <firat.oezcan@gmail.com>
2020-08-27 07:52:01 +0200

    Disabled placeholder image

--- components/Image.tsx
+++ components/Image.tsx
@@ -10,7 +10,7 @@ export const Image = ({ src, className, alt }: Props) => {
   const image = getImage(src);
   return (
     <div className={className}>
-      <img className="block w-full" src={image.placeholder} />
+      {/* <img className="block w-full" src={image.placeholder} alt={`Placeholder - ${alt}`} /> */}
       <img className="block -mt-full" srcSet={image.srcSet} src={image.src} alt={alt || ""} loading="lazy" />
     </div>
   );
