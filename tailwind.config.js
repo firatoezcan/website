64d09d5a603cec36ee8a9a3285620c71d5850993
Firat Özcan <firat.oezcan@gmail.com>
2020-11-02 16:00:02 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -692,6 +692,49 @@ module.exports = {
         },
       },
     },
+  typography: {
+    default: {
+      css: {
+        color: theme('colors.gray.300'),
+                    a: {
+                        color: theme('colors.green.500'),
+                        '&:hover': {
+                            color: theme('colors.green.500'),
+                        },
+                    },
+
+                    h1: {
+                        color: theme('colors.gray.300'),
+                    },
+                    h2: {
+                        color: theme('colors.gray.300'),
+                    },
+                    h3: {
+                        color: theme('colors.gray.300'),
+                    },
+                    h4: {
+                        color: theme('colors.gray.300'),
+                    },
+                    h5: {
+                        color: theme('colors.gray.300'),
+                    },
+                    h6: {
+                        color: theme('colors.gray.300'),
+                    },
+
+                    strong: {
+                        color: theme('colors.gray.300'),
+                    },
+
+                    code: {
+                        color: theme('colors.gray.300'),
+                    },
+
+                    figcaption: {
+                        color: theme('colors.gray.500'),
+                    },
+                },
+}}
   },
   variants: {
     accessibility: ['responsive', 'focus'],
